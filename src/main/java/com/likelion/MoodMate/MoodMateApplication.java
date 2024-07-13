a3b2658ffce739b11dde7e4b5cb011ffce8bd7bc
daeminkim <kdm0068@gmail.com>
2024-07-13 20:14:14 +0900

    first commit

---
+++ src/main/java/com/likelion/MoodMate/MoodMateApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.MoodMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodMateApplication.class, args);
+	}
+
+}
